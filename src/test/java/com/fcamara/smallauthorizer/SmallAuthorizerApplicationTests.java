e7a0c03ff43200bdba76f985a5853d62d3e60dfe
Claudemir <claudemirramosferreira@gmail.com>
2024-12-16 21:12:50 -0300

    commit initial

---
+++ src/test/java/com/fcamara/smallauthorizer/SmallAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamara.smallauthorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
