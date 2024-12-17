3d0ad807b11d86f5802f7434c1ca7fbc6e201530
Claudemir <claudemirramosferreira@gmail.com>
2024-12-16 21:05:11 -0300

    commit initial

---
+++ src/main/java/com/fcamara/smallauthorizer/SmallAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package com.fcamara.smallauthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallAuthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallAuthorizerApplication.class, args);
+	}
+
+}
